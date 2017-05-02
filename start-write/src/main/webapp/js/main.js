e26c23742078c852e05a9a6bae1c818486b69770
miolivc <miolivc>
2017-05-02 20:47:57 -0300

    Adicionando menu principal

--- start-write/src/main/webapp/js/main.js
+++ start-write/src/main/webapp/js/main.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,5 +9,13 @@ addEventListener("load", function() {
 }, false);
 
 function hideURLbar(){
-    window.scrollTo(0,1); 
-} 
+    window.scrollTo(0,1);
+}
+
+function criarBlog(){
+    window.location.href = "criarBlog.html";
+}
+
+function listarBlog(){
+    window.location.href = "blogs.html";
+}
