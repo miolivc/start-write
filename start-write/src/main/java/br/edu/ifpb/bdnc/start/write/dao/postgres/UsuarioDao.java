9b86f966534e69a75f6f923539797690bcbf91f3
miolivc <miolivc>
2017-05-02 23:17:59 -0300

    Persistindo user no postgres

--- start-write/src/main/java/br/edu/ifpb/bdnc/start/write/dao/postgres/UsuarioDao.java
+++ start-write/src/main/java/br/edu/ifpb/bdnc/start/write/dao/postgres/UsuarioDao.java
@@ -5,9 +5,16 @@
  */
 package br.edu.ifpb.bdnc.start.write.dao.postgres;
 
+import br.edu.ifpb.bdnc.start.write.factory.PostgresConnectionFactory;
 import br.edu.ifpb.bdnc.start.write.dao.UsuarioDaoIf;
 import br.edu.ifpb.bdnc.start.write.model.Usuario;
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.List;
 
 /**
@@ -15,30 +22,97 @@ import java.util.List;
  * @author alann
  */
 public class UsuarioDao implements UsuarioDaoIf {
+    private Connection conn = null;
+
+    public UsuarioDao() throws SQLException, ClassNotFoundException{
+	conn = PostgresConnectionFactory.getConnection();
+    }
 
     @Override
     public void add(Usuario pessoa) {
-        
+        String sql = "INSERT INTO USUARIO(id,name, birthDate, username, email, password)"
+                + "VALUES (?,?,?,?,?,?)";
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, pessoa.getId());
+            stmt.setString(2, pessoa.getName());
+            stmt.setDate(3, Date.valueOf(pessoa.getBirthDate().toString()));
+            stmt.executeUpdate();
+            stmt.close();
+        } catch (SQLException ex) {
+            System.err.println(ex.toString());
+        }
     }
 
     @Override
     public void delete(String email) {
-        
+        String sql = "DELETE FROM USUARIO WHERE EMAIL = ?";
+        try {
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, email);
+            stmt.executeUpdate();
+            stmt.close();
+        } catch (SQLException ex) {
+            System.err.println(ex.toString());
+        }
     }
 
     @Override
     public List<Usuario> list() {
-        return null;
+        List<Usuario> users = null;
+        String sql = "SELECT * FROM USUARIO ";
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                Usuario user = new Usuario();
+                user.setId(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                user.setBirthDate(rs.getDate("birthDate"));
+                user.setUsername(rs.getString("username"));
+                user.setEmail(rs.getString("email"));
+                user.setPassword(rs.getString("password"));
+                users.add(user);
+            }
+            rs.close();
+            stmt.close();
+        } catch (SQLException ex) {
+            System.err.println(ex.toString());
+        }
+        return users;
     }
 
     @Override
     public Usuario find(String email) {
-        return null;
+        Usuario user = null;
+        String sql = "SELECT * FROM USUARIO ";
+        try {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()){
+                user.setId(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                user.setBirthDate(rs.getDate("birthDate"));
+                user.setUsername(rs.getString("username"));
+                user.setEmail(rs.getString("email"));
+                user.setPassword(rs.getString("password"));
+            }
+            rs.close();
+            stmt.close();
+        } catch (SQLException ex) {
+            System.err.println(ex.toString());
+        }
+        return user;
     }
 
     @Override
     public void update(Usuario pessoa) {
-        
+        try{
+            delete(pessoa.getEmail());
+            add(pessoa);
+        } catch (Exception ex){
+            System.err.println(ex.toString());
+        }
     }
     
 }
