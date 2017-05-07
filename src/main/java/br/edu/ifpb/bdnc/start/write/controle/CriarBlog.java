bbe8b0d298badaccfb773171af4b41948b3fc7b9
alanncz <alannrodrigues@hotmail.com>
2017-05-07 15:05:05 -0300

    Correção Parametros

--- src/main/java/br/edu/ifpb/bdnc/start/write/controle/CriarBlog.java
+++ src/main/java/br/edu/ifpb/bdnc/start/write/controle/CriarBlog.java
@@ -13,7 +13,6 @@ import java.io.IOException;
 import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
@@ -22,7 +21,6 @@ import javax.servlet.http.Part;
  *
  * @author miolivc
  */
-@MultipartConfig
 public class CriarBlog implements Comando {
 
     @Override
